ceae90acbdec7395ed1b3be976866eb005e46f1e
B4g4rini <pedro.bagarini2000@gmail.com>
2024-08-29 17:01:47 -0300

    Continuação da página sobre nos

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,16 +1,15 @@
-import './Cards.module.css'; 
+import styles from "./Cards.module.css";
+
+const Cards = ({ img, nome }) => {
+  return (
+    <div className={styles.Sobre}>
+        <div className={styles.ContainerCard}>
+    <div className={styles.Card}><img src= {img} alt={nome} /></div>
 
 
+    </div>
+  </div>
+  );
+};
 
-
-function Cards({ title, image, description }) {
-    return (
-      <div className="card">
-        <img src={image} alt={title} />
-        <h2>{title}</h2>
-        <p>{description}</p>
-      </div>
-    );
-  }
-  
-  export default Cards;
+export default Cards;
--- client/src/routes/Sobre/Sobre.jsx
+++ client/src/routes/Sobre/Sobre.jsx
@@ -1,129 +1,171 @@
 import Cards from "../../components/Cards/Cards";
 import styles from "./Sobre.module.css";
 
-
-import React from 'react';
-
 function Sobre() {
   return (
     <section id="quem-somos">
       <div className={styles.sobre}>
-      <h2>Quem somos nós?</h2>
-      <p>
-        {/* Substitua o Lorem Ipsum por seu texto real */}
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,   
- quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.   
+        <h2>Quem somos nós?</h2>
+        <p>APRESENTAÇÃO</p>
+        <h3>Equipe</h3>
 
-      </p>   
+        <div className="equipe">
 
 
-      <h3>Equipe</h3>
-      <div className="equipe">
-        <div className="membro">
+
+          <div className="membro">
+            <div className={styles.Card1}>
+              <Cards></Cards>
+              <h4>Anelize</h4>
+              <p>
+                Estudante, dançarina e programadora, meu nome é Anelize
+                Nardelli, eu tenho 17 anos de idade e estou cursando o Ensino
+                Médio Técnico em Análise e Desenvolvimento de Sistemas no SENAI
+                Jau. Atuando como Líder e Gestora de Projetos da Not a Number,
+                tenho experiência com gerenciamento de equipes e projetos a
+                partir de metodologias ágeis. Além de tudo, sou apaixonada por
+                tecnologia e tenho experiência como desenvolvedora Full Stack.
+              </p>
+              <div></div>
+              <div className={styles.Social1}>
+                <a className={styles.ins} href="https://www.instagram.com/">
+                  <ion-icon name="logo-instagram"></ion-icon>
+                </a>
+                <a className={styles.lin} href="https://www.linkedin.com/">
+                  <ion-icon name="logo-linkedin"></ion-icon>
+                </a>
+                <a className={styles.git} href="https://github.com/">
+                  <ion-icon name="logo-github"></ion-icon>
+                </a>
+              </div>
+            </div>
+          </div>
+
+
+
+
+
+       
+            <div className={styles.Card2}>
+              <Cards></Cards>
+              <h4>Cauã Diório</h4>
+              <p>
+                Estudante, dançarina e programadora, meu nome é Anelize
+                Nardelli, eu tenho 17 anos de idade e estou cursando o Ensino
+                Médio Técnico em Análise e Desenvolvimento de Sistemas no SENAI
+                Jau. Atuando como Líder e Gestora de Projetos da Not a Number,
+                tenho experiência com gerenciamento de equipes e projetos a
+                partir de metodologias ágeis. Além de tudo, sou apaixonada por
+                tecnologia e tenho experiência como desenvolvedora Full Stack.
+              </p>
+              <div>
+              </div>
+              <div className={styles.Social1}>
+                <a className={styles.ins} href="https://www.instagram.com/">
+                  <ion-icon name="logo-instagram"></ion-icon>
+                </a>
+                <a className={styles.lin} href="https://www.linkedin.com/">
+                  <ion-icon name="logo-linkedin"></ion-icon>
+                </a>
+                <a className={styles.git} href="https://github.com/">
+                  <ion-icon name="logo-github"></ion-icon>
+                </a>
+              </div>
+            
+          </div>
+
+
+
+
+
+         
+            {/* <div className={styles.Card3}>
+              <Cards></Cards>
+              <h4>Felipe Salmazo</h4>
+              <p>
+                Estudante, dançarina e programadora, meu nome é Anelize
+                Nardelli, eu tenho 17 anos de idade e estou cursando o Ensino
+                Médio Técnico em Análise e Desenvolvimento de Sistemas no SENAI
+                Jau. Atuando como Líder e Gestora de Projetos da Not a Number,
+                tenho experiência com gerenciamento de equipes e projetos a
+                partir de metodologias ágeis. Além de tudo, sou apaixonada por
+                tecnologia e tenho experiência como desenvolvedora Full Stack.
+              </p>
+              <div></div>
+              <div className={styles.Social1}>
+                <a className={styles.ins} href="https://www.instagram.com/">
+                  <ion-icon name="logo-instagram"></ion-icon>
+                </a>
+                <a className={styles.lin} href="https://www.linkedin.com/">
+                  <ion-icon name="logo-linkedin"></ion-icon>
+                </a>
+                <a className={styles.git} href="https://github.com/">
+                  <ion-icon name="logo-github"></ion-icon>
+                </a>
+              </div>
+            </div>
+      
           
-          <p>
-            Estudante, dançarina e programadora, meu nome é Anelize Nardelli, eu tenho 17 anos de idade e estou cursando o Ensino Médio Técnico em Análise e Desenvolvimento de Sistemas no SENAI Jau. Atuando como Líder e Gestora de Projetos da Not a Number, tenho experiência com gerenciamento de equipes e projetos a partir de metodologias ágeis. Além de tudo, sou apaixonada por tecnologia e tenho experiência como desenvolvedora Full Stack.
-          </p>
-          <div className="redes-sociais">
-            <a href="#" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-linkedin"></i>
-            </a>
-            <a href="#" target="_blank" rel="noopener noreferrer">
-              <i className="fab   
- fa-github"></i>
-            </a>
-            <a href="#" target="_blank" rel="noopener noreferrer">
-              <i className="far   
- fa-envelope"></i>
-            </a>
+
+
+
+
+            <div className={styles.Card4}>
+              <Cards></Cards>
+              <h4>Gustavo Menegassi Lara</h4>
+              <p>
+                Estudante, dançarina e programadora, meu nome é Anelize
+                Nardelli, eu tenho 17 anos de idade e estou cursando o Ensino
+                Médio Técnico em Análise e Desenvolvimento de Sistemas no SENAI
+                Jau. Atuando como Líder e Gestora de Projetos da Not a Number,
+                tenho experiência com gerenciamento de equipes e projetos a
+                partir de metodologias ágeis. Além de tudo, sou apaixonada por
+                tecnologia e tenho experiência como desenvolvedora Full Stack.
+              </p>
+              <div></div>
+              <div className={styles.Social1}>
+                <a className={styles.ins} href="https://www.instagram.com/">
+                  <ion-icon name="logo-instagram"></ion-icon>
+                </a>
+                <a className={styles.lin} href="https://www.linkedin.com/">
+                  <ion-icon name="logo-linkedin"></ion-icon>
+                </a>
+                <a className={styles.git} href="https://github.com/">
+                  <ion-icon name="logo-github"></ion-icon>
+                </a>
+              </div>
+            </div>
+
+
+
+
+            <div className={styles.Card5}>
+              <Cards></Cards>
+              <h4>Pedro Gonçalves Bagarini</h4>
+              <p>
+                Estudante, dançarina e programadora, meu nome é Anelize
+                Nardelli, eu tenho 17 anos de idade e estou cursando o Ensino
+                Médio Técnico em Análise e Desenvolvimento de Sistemas no SENAI
+                Jau. Atuando como Líder e Gestora de Projetos da Not a Number,
+                tenho experiência com gerenciamento de equipes e projetos a
+                partir de metodologias ágeis. Além de tudo, sou apaixonada por
+                tecnologia e tenho experiência como desenvolvedora Full Stack.
+              </p>
+              <div></div>
+              <div className={styles.Social1}>
+                <a className={styles.ins} href="https://www.instagram.com/">
+                  <ion-icon name="logo-instagram"></ion-icon>
+                </a>
+                <a className={styles.lin} href="https://www.linkedin.com/">
+                  <ion-icon name="logo-linkedin"></ion-icon>
+                </a>
+                <a className={styles.git} href="https://github.com/">
+                  <ion-icon name="logo-github"></ion-icon>
+                </a>
+              </div>
+            </div> */}
           </div>
         </div>
-
-
-
-        
-        <div className="membro">
-      
-          <p>
-            Estudante, dançarina e programadora, meu nome é Anelize Nardelli, eu tenho 17 anos de idade e estou cursando o Ensino Médio Técnico em Análise e Desenvolvimento de Sistemas no SENAI Jau. Atuando como Líder e Gestora de Projetos da Not a Number, tenho experiência com gerenciamento de equipes e projetos a partir de metodologias ágeis. Além de tudo, sou apaixonada por tecnologia e tenho experiência como desenvolvedora Full Stack.
-          </p>
-          <div>
-            <a  className={styles.ins}  href="https://www.instagram.com/">
-              <ion-icon name="logo-instagram"></ion-icon>
-            </a>
-            <a  className={styles.lin} href="https://www.linkedin.com/">
-              <ion-icon name="logo-linkedin"></ion-icon>
-            </a>
-            <a  className={styles.git} href="https://github.com/">
-              <ion-icon name="logo-github"></ion-icon>
-            </a>
-            </div>
-      </div>
-
-
-
-
-      <div className="membro">
-          <h4>Cauã Diório</h4>
-          <p>
-            Estudante, dançarina e programadora, meu nome é Anelize Nardelli, eu tenho 17 anos de idade e estou cursando o Ensino Médio Técnico em Análise e Desenvolvimento de Sistemas no SENAI Jau. Atuando como Líder e Gestora de Projetos da Not a Number, tenho experiência com gerenciamento de equipes e projetos a partir de metodologias ágeis. Além de tudo, sou apaixonada por tecnologia e tenho experiência como desenvolvedora Full Stack.
-          </p>
-          <div>
-            <a  className={styles.ins}  href="https://www.instagram.com/">
-              <ion-icon name="logo-instagram"></ion-icon>
-            </a>
-            <a  className={styles.lin} href="https://www.linkedin.com/">
-              <ion-icon name="logo-linkedin"></ion-icon>
-            </a>
-            <a  className={styles.git} href="https://github.com/">
-              <ion-icon name="logo-github"></ion-icon>
-            </a>
-            </div>
-      </div>
-
-
-
-
-      <div className="membro">
-          <h4>Cauã Diório</h4>
-          <p>
-            Estudante, dançarina e programadora, meu nome é Anelize Nardelli, eu tenho 17 anos de idade e estou cursando o Ensino Médio Técnico em Análise e Desenvolvimento de Sistemas no SENAI Jau. Atuando como Líder e Gestora de Projetos da Not a Number, tenho experiência com gerenciamento de equipes e projetos a partir de metodologias ágeis. Além de tudo, sou apaixonada por tecnologia e tenho experiência como desenvolvedora Full Stack.
-          </p>
-          <div>
-            <a  className={styles.ins}  href="https://www.instagram.com/">
-              <ion-icon name="logo-instagram"></ion-icon>
-            </a>
-            <a  className={styles.lin} href="https://www.linkedin.com/">
-              <ion-icon name="logo-linkedin"></ion-icon>
-            </a>
-            <a  className={styles.git} href="https://github.com/">
-              <ion-icon name="logo-github"></ion-icon>
-            </a>
-            </div>
-      </div>
-
-
-
-
-      <div className="membro">
-          <h4>Cauã Diório</h4>
-          <p>
-            Estudante, dançarina e programadora, meu nome é Anelize Nardelli, eu tenho 17 anos de idade e estou cursando o Ensino Médio Técnico em Análise e Desenvolvimento de Sistemas no SENAI Jau. Atuando como Líder e Gestora de Projetos da Not a Number, tenho experiência com gerenciamento de equipes e projetos a partir de metodologias ágeis. Além de tudo, sou apaixonada por tecnologia e tenho experiência como desenvolvedora Full Stack.
-          </p>
-          <div>
-            <a  className={styles.ins}  href="https://www.instagram.com/">
-              <ion-icon name="logo-instagram"></ion-icon>
-            </a>
-            <a  className={styles.lin} href="https://www.linkedin.com/">
-              <ion-icon name="logo-linkedin"></ion-icon>
-            </a>
-            <a  className={styles.git} href="https://github.com/">
-              <ion-icon name="logo-github"></ion-icon>
-            </a>
-            </div>
-      </div>
-      </div>
-      </div>
     </section>
   );
 }
