f28241e32ea117c7fc90440f281cc996850de274
di0rio <caua.diorio29@gmail.com>
2024-08-30 11:30:47 -0300

    adicionando bootstrap no projeto e refazendo o header

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import App from "./App.jsx";
-
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
